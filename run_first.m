eb9abd4164b5b78645c71286d62e48306a02086f
Justin H <heliopause@gmail.com>
2013-04-19 11:30:22 -0700

    fixed previous files

--- run_first.m
+++ run_first.m
@@ -8,7 +8,7 @@ function [imageHistograms,imageBitDepth] = run_first(inputDirectory,outputDirect
 %
 % The purpose of this function is to perform flip over horizontal axis of 
 % the image to correct for issues in the TIFF library used to capture images
-% (see SoftTriggerOSC.cpp). The function will also move the input files into 
+% (see SoftTriggerOSC.cpp). The function will also copy the input files to 
 % a working directory to prevent overwriting of original data and minimizing
 % use of the original data disk. The return values are the histogram counts 
 % and the image bit depth.
@@ -25,34 +25,36 @@ assert(isdir(outputDirectory),'Not a valid output directory.');
 assert(numel(imformats(imageExtension))==1,'Not a valid image file format.');
 
 imageOutputList = dir([outputDirectory '*.' imageExtension]);
+continueOrNot = 'y';
 if ~isempty(imageOutputList)
     continueOrNot = input(['Output directory contains ' upper(imageExtension)...
         ' files. Continue with processing [y/n]?\n'],'s');
-    if strcmp(continueOrNot,'y')
-        disp('Continuing the process...');
-        imageInputList = dir([inputDirectory '*.' imageExtension]);
-        imageInfo = imfinfo([inputDirectory imageInputList(1).name],imageExtension);
-        imageBitDepth = imageInfo.BitDepth;
-        
-        nImages = numel(imageInputList);
-        imageHistograms = nan(2^imageBitDepth,nImages);
-        for iImage = 1:nImages
-            imageName = imageInputList(iImage).name;
-            imageTemp = imread([inputDirectory imageName]);
+end
 
-            imageTemp = flipud(imageTemp);
-            
-            imageHistogram = imhist(imageTemp,2^imageBitDepth);
-            imageHistograms(:,iImage) = imageHistogram;
-            
-            imwrite(imageTemp,[outputDirectory imageName],imageExtension);
-        end
-        disp('Completed the process.');
-    else 
-        disp('Cancelling the process.');
-        imageHistograms = nan;
-        imageBitDepth = nan;
+if ~strcmp(continueOrNot,'y')
+    disp('Cancelling the process.');
+    imageHistograms = nan;
+    imageBitDepth = nan;
+else
+    disp('Processing files...');
+    imageInputList = dir([inputDirectory '*.' imageExtension]);
+    imageInfo = imfinfo([inputDirectory imageInputList(1).name],imageExtension);
+    imageBitDepth = imageInfo.BitDepth;
+    
+    nImages = numel(imageInputList);
+    imageHistograms = nan(2^imageBitDepth,nImages);
+    for iImage = 1:nImages
+        imageName = imageInputList(iImage).name;
+        imageTemp = imread([inputDirectory imageName]);
+        
+        imageTemp = flipud(imageTemp);
+        
+        imageHistogram = imhist(imageTemp,2^imageBitDepth);
+        imageHistograms(:,iImage) = imageHistogram;
+        
+        imwrite(imageTemp,[outputDirectory imageName],imageExtension);
     end
+    disp('Completed the process.');
 end
 
 end
--- test_script.m
+++ test_script.m
@@ -5,8 +5,10 @@ close all; clear all; clc;
 % load config/parameter file OR specify input arguments
 % test run_first with directory input
 
-inputDirectory = 'TEST DATA/inputDirectory/';
-outputDirectory = 'TEST DATA/outputDirectory/';
-imageExtension = 'tif';
+inputDirectory = '/Users/justin/Desktop/Hatchetfish test data/Spc16a-5 (red-air, mid-behind-head, 2x2, .4, 17ms, 2-10, g0)/';
+outputDirectory = '/Users/justin/Desktop/test/';
+% inputDirectory = 'TEST DATA/inputDirectory/';
+% outputDirectory = 'TEST DATA/outputDirectory/';
+imageExtension = 'tiff';
 
 [imageHistograms,imageBitDepth] = run_first(inputDirectory,outputDirectory,imageExtension);
